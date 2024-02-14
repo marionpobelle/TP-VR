de9038a4245e858cff1905a982c9112d22af2b1c
Lyrdinn <90039770+Lyrdinn@users.noreply.github.com>
2024-02-14 15:28:31 +0100

    Spider shooting animation added

--- Assets/Scripts/AnimateHands.cs
+++ Assets/Scripts/AnimateHands.cs
@@ -6,50 +6,12 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Animator))]
 public class HandAnimator : MonoBehaviour
 {
-    public InputActionReference gripInputActionReference;
-    public InputActionReference triggerInputActionReference;
 
     private Animator _handAnimator;
-    private float _gripValue;
-    private float _triggerValue;
 
     private void Awake()
     {
         _handAnimator = GetComponent<Animator>();
     }
 
-    private void Update()
-    {
-        AnimateGrip();
-        AnimateTrigger();
-    }
-
-    private void AnimateGrip()
-    {
-        _gripValue = gripInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Grip", _gripValue);
-    }
-
-    private void AnimateTrigger()
-    {
-        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Grip", _triggerValue);
-    }
-
-
-
-    private void OnDestroy()
-    {
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-
-    }
 }
